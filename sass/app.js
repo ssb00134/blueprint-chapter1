130726f52996ce80d23f9377d7a6e887486a468f
jun <ssb00134@naver.com>
2021-09-14 22:33:32 +0900

    app 구조 변경

--- sass/app.js
+++ sass/app.js
@@ -3,30 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sass = require('node-sass');
-var route = require('./server/routes/index');
-var users = require('./server/routes/users');
 
-
-var indexRouter = require('./server/routes/index');
-var usersRouter = require('./server/routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'server/views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(sass({
-//   src: path.join(__dirname, 'public'),
-//   dest: path.join(__dirname, 'public'),
-//   indentedSyntax: true, // true = .sass and false = .scss
-//   sourceMap: true
-// }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
